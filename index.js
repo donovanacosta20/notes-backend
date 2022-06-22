77731e1da1c4b6e5072ae5f1a15bc2197d79a4d3
donovanacosta20 <dnvnacosta81@gmail.com>
2022-06-22 09:53:32 -0300

    add database path to heroku

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
 app.use(express.static('build'));
 
-require('dotenv').config();
+
 const Note = require('./models/note')
 
 app.use(express.json());
